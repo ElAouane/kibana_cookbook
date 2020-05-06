c13e5bceb50f306ea50f32de0cd4c123e227715d
elaouane <hamzagamedev@hotmail.com>
2020-05-06 03:08:58 +0100

    fixed cookbook

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,7 +1,7 @@
 
 require 'spec_helper'
 
-describe 'kibana_cookbook::default' do
+describe 'kibana::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
