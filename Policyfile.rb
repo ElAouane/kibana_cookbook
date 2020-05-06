c7698cc705431fdd7da2d376884a205686e3bedc
elaouane <hamzagamedev@hotmail.com>
2020-05-06 03:05:10 +0100

    initial commit

--- Policyfile.rb
+++ Policyfile.rb
@@ -10,7 +10,7 @@ name 'kibana'
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'kibana::default'
+run_list 'kibana_cookbook::default'
 
 # Specify a custom source for a single cookbook:
-cookbook 'kibana', path: '.'
+cookbook 'kibana_cookbook', path: '.'
